8ba681b37c3d2874555f9331d111e66487ed3183
youngguy1998 <chenfeng1597@163.com>
2019-10-13 22:26:48 +0800

    这是一个封装好的九九乘法表函数

---
+++ cf3.py
@@ -0,0 +1,11 @@
+def multiple():
+    # 九九乘法表函数
+    row = 1
+    while row < 10:
+        col = 1
+        while col <= row:
+            result = row * col
+            print('{} * {} = {}'.format(row, col, result), end='\t')
+            col += 1
+        print('')
+        row += 1
